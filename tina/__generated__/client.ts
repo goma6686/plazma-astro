3d1e16616479743f14065746a6f113d7a86f1cfa
goma6686 <maciuolyte@gmail.com>
2025-05-10 15:44:26 +0000

    iiiiiiiiiiiiiiiiii

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'b6e71e0c651165fe54e1a9a5070f65d4fd2d36bc', queries,  });
-export default client;
-  
